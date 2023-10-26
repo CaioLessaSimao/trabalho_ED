c739c4e00eb39dcfd2fb67802d826845da9220fa
MountSome <caiolessa02@gmail.com>
2023-10-25 23:53:06 -0300

    fim

--- image.c
+++ image.c
@@ -1,135 +1,220 @@
 #include "image.h"
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct image_struct {
-	char tipo[3];
-	int colunas;
-	int linhas;
-	unsigned char **pixels;
+  char tipo[3];
+  int colunas;
+  int linhas;
+  unsigned char ***pixels;
 };
 
+void imprimirMatriz(image *pic) {
+  if (pic == NULL) {
+    printf("Impossível imprimir a matriz, a imagem não foi carregada "
+           "corretamente.\n");
+    return;
+  }
 
-image* create(int rows, int cols, const char type[]){ //adicionar rgb
-	unsigned char **matriz = malloc(rows * sizeof(unsigned char *));
-	
-	for (int k = 0; k < rows; k++) {
-    matriz[k] = malloc(cols * sizeof(unsigned char));
-  	}
+  printf("Tipo: %s\n", pic->tipo);
+  printf("Dimensões: %d x %d\n", pic->colunas, pic->linhas);
 
-	srand(time(NULL));
-	int random;
-
-	/*
-  	for (int i = 0; i < rows; i++) {
-    	for (int j = 0; j < cols; j++) {
-    		random = rand() % 256;
-    		matriz[i][j] = (unsigned char)random;
-    	}
- 	}
-	*/
- 	
-	image *pic = malloc(sizeof(image));
-
-	strcpy(pic->tipo, type);
-	pic->colunas = cols;
-	pic->linhas = rows;
-	pic->pixels = matriz;
-	
-
-	return pic;
+  // Percorra a matriz e imprima os valores de cada pixel
+  for (int i = 0; i < pic->linhas; i++) {
+    for (int j = 0; j < pic->colunas; j++) {
+      printf("(%3u, %3u, %3u) ", pic->pixels[i][j][0], pic->pixels[i][j][1],
+             pic->pixels[i][j][2]);
+    }
+    printf("\n");
+  }
 }
 
+image *create(int linhas, int colunas, const char type[]) {
+  image *pic = malloc(sizeof(image));
+  strcpy(pic->tipo, type);
+  pic->colunas = colunas;
+  pic->linhas = linhas;
 
-image* load_from_ppm(const char* filename){
+  unsigned char ***matriz = (unsigned char ***)malloc(linhas * sizeof(unsigned char **));
 
-	FILE* ptr = fopen(filename, "a+");
+  srand(time(NULL));
 
-	char type[4];
-	fgets(type, 20, ptr);
+  // aloca memoria pro tipo p2
+  if (strcmp(type, "P2") == 0) {
+    for (int i = 0; i < linhas; i++) {
+      matriz[i] = (unsigned char **)malloc(colunas * sizeof(unsigned char *));
 
-	char tamanho[10];
-	fgets(tamanho, 50, ptr);
+      // aloca memoria pros pixels
+      for (int j = 0; j < colunas; j++) {
+        matriz[i][j] = (unsigned char *)malloc(1 * sizeof(unsigned char));
 
-	char* coluna = strtok(tamanho, " ");
-    char* linha = strtok(NULL, " ");
+        // preenche com valores aleatórios
+        int random = rand() % 256;
+        matriz[i][j][0] = (unsigned char)random;
+      }
+    }
 
-    int cols = atoi(coluna);
-    int rows = atoi(linha);
+    // aloca memoria pro tipo p3
+  } else if (strcmp(type, "P3") == 0) {
+    for (int i = 0; i < linhas; i++) {
+      matriz[i] = (unsigned char **)malloc(colunas * sizeof(unsigned char *));
 
-    unsigned char **matriz = malloc(rows * sizeof(unsigned char *));
-	
-	for (int k = 0; k < rows; k++) {
-    	matriz[k] = malloc(cols * sizeof(unsigned char));
-  	}
+      // aloca memoria pros pixels
+      for (int j = 0; j < colunas; j++) {
+        unsigned char rgb[3];
 
-  	fgets(tamanho, 50, ptr);
+        // preenche vetor RGB com valores aleatórios
+        for (int k = 0; k < 3; k++) {
+          int random = rand() % 256;
+          rgb[k] = (unsigned char)random;
+        }
 
-  	char *elemento = malloc(3*sizeof(char));
-  	char aux2[50];
-  	int convertido = 0;  //mudar para unsigned char
-  	
-  	for(int i = 0; i < rows; i++){ 		
-  		
-  		fgets(aux2, 50, ptr);
-  		elemento = strtok(aux2, " ");
-  		convertido = atoi(elemento); 
-  		matriz[i][0] = (unsigned char)convertido;
-  		
-  		for(int j = 1; j < cols; j++){
-  			
-  			elemento = strtok(NULL, " ");
-  			convertido = atoi(elemento);
-  			matriz[i][j] = (unsigned char)convertido;
-  		}
-  	}
- 
-  	image *pic = malloc(sizeof(image));
+        // aloca a memória pro pixel e preenche
+        matriz[i][j] = (unsigned char *)malloc(3 * sizeof(unsigned char));
+        memcpy(matriz[i][j], rgb, 3);
+      }
+    }
+  }
 
-	strcpy(pic->tipo, type);
-	pic->colunas = cols;
-	pic->linhas = rows;
-	pic->pixels = matriz;
+  pic->pixels = matriz;
 
-	printf("%s", pic->tipo);
-	printf("%d\n", pic->colunas);
-	printf("%d\n", pic->linhas);
-
-	return pic;
+  return pic;
 }
 
+// lê um arquivo PPM do tipo P3 e retorna uma estrutura imagem
+image *load_from_ppm(const char *filename) {
 
-void write_to_ppm(image* image, const char* filename){
-	
-	FILE *file = fopen(filename, "w");
-  	if (!file) {
-    	fprintf(stderr, "Error opening file for writing: %s\n", filename);
-    	return;
-  	}
+  FILE *ptr = fopen(filename, "a+");
 
+  if (ptr == NULL) {
+    printf("Erro ao abrir o arquivo\n");
+    return NULL;
+  }
 
-	int width = image->colunas;
-	int height = image->linhas;
+  // Primeira linha
+  char linha[1000];
+  fgets(linha, 1000, ptr);
+  char tipo[4];
+  sscanf(linha, "%3s", tipo);
 
-	fprintf(file, "P2\n");
-	fprintf(file, "%d %d\n", width, height);
-	fprintf(file, "255\n"); 
+  // Segunda linha
+  fgets(linha, 1000, ptr);
+  int linhas, colunas;
+  sscanf(linha, "%d %d", &colunas, &linhas);
 
+  // Terceira linha
+  fgets(linha, 1000, ptr);
+  int maximo;
+  sscanf(linha, "%d", &maximo);
 
-  	for (int i = 0; i < height; i++) {
-    	for (int j = 0; j < width; j++) {
-      		fprintf(file, "%d", image->pixels[i][j]);
-      		if (j < width - 1) {
-       			fprintf(file, " ");
-      		}
-      		else {
-        		fprintf(file, "\n");
-      		}
-    	}
-  	}
-  	fclose(file);
+  // Criando a matriz
+  unsigned char ***matriz =
+      (unsigned char ***)malloc(linhas * sizeof(unsigned char **));
+  for (int i = 0; i < linhas; i++) {
+    matriz[i] = (unsigned char **)malloc(colunas * sizeof(unsigned char *));
+    for (int j = 0; j < colunas; j++) {
+      matriz[i][j] = (unsigned char *)malloc(3 * sizeof(unsigned char));
+    }
+  }
+
+  // Lendo e preenchendo os valores RGB
+  for (int i = 0; i < linhas; i++) {
+    for (int j = 0; j < colunas; j++) {
+      unsigned char r, g, b;
+      if (fscanf(ptr, "%hhu %hhu %hhu", &r, &g, &b) == 3) {
+        matriz[i][j][0] = r;
+        matriz[i][j][1] = g;
+        matriz[i][j][2] = b;
+      } else {
+        printf("Erro na leitura dos valores RGB.\n");
+      }
+    }
+  }
+
+  image *pic = malloc(sizeof(image));
+  strcpy(pic->tipo, tipo);
+  pic->colunas = colunas;
+  pic->linhas = linhas;
+  pic->pixels = matriz;
+
+  fclose(ptr);
+
+  // imprimindo antes de retornar
+  // imprimirMatriz(pic);
+  return pic;
 }
 
-//criar função usando scanf()
+void rgb_to_gray(image *image_rgb, image *image_gray) {
+  
+  // Copia os campos da imagem RGB
+  *image_gray = *image_rgb;
+  strcpy(image_gray->tipo, "P2");
+
+  for (int i = 0; i < image_rgb->linhas; i++) {
+    
+    for (int j = 0; j < image_rgb->colunas; j++) {
+
+      //pegando da matriz os valores RGB
+      unsigned char r = image_rgb->pixels[i][j][0];
+      unsigned char g = image_rgb->pixels[i][j][1];
+      unsigned char b = image_rgb->pixels[i][j][2];
+
+      //convertendo pra cinza
+      unsigned char intensidade = (unsigned char)(0.299 * r + 0.587 * g + 0.114 * b);
+      image_gray->pixels[i][j][0] = intensidade;
+    }
+  }
+}
+
+void write_to_ppm(image *image, const char *filename) {
+    FILE *file = fopen(filename, "w");
+
+    if (!file) {
+        fprintf(stderr, "Erro ao abrir o arquivo para escrita: %s\n", filename);
+        return;
+    }
+
+    int colunas = image->colunas;
+    int linhas = image->linhas;
+
+    //Imagem P2
+    if (strcmp(image->tipo, "P2") == 0) {
+        fprintf(file, "P2\n");
+        fprintf(file, "%d %d\n", colunas, linhas);
+        fprintf(file, "255\n");
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                fprintf(file, "%d", image->pixels[i][j][0]);
+                if (j < colunas - 1) {
+                    fprintf(file, " ");
+                } else {
+                    fprintf(file, "\n");
+                }
+            }
+        }
+
+    //Imagem p3
+    } else if (strcmp(image->tipo, "P3") == 0) {
+        fprintf(file, "P3\n");
+        fprintf(file, "%d %d\n", colunas, linhas);
+        fprintf(file, "255\n");
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                fprintf(file, "%d %d %d", image->pixels[i][j][0], image->pixels[i][j][1], image->pixels[i][j][2]);
+                if (j < colunas - 1) {
+                    fprintf(file, " ");
+                } else {
+                    fprintf(file, "\n");
+                }
+            }
+        }
+    } else {
+        fprintf(stderr, "Formato de imagem não suportado: %s\n", image->tipo);
+    }
+
+    fclose(file);
+}
--- main.c
+++ main.c
@@ -1,12 +1,60 @@
 #include "image.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
 
-	//image *pic = create(3,3,"P2");
-	//write_to_ppm(pic, "a.ppm");
-	//load_from_ppm("a.ppm");
+	int opcao;
+	int escolha;
+	char *arquivo = malloc(20 * sizeof(char));
+	image *figura = malloc(sizeof(image*));
+
+  	image *cinza = malloc(sizeof(image *));
+
+	printf("Escolha sua opcao: \n");
+	printf("1) Carregar imagem\n" );
+	printf("2) Converter imagem para cinza\n");
+	printf("3) Escrever imagem\n" );
+	printf("4) Sair\n");
+	printf("Digite sua opcao: ");
+	scanf("%d", &opcao);
+
+	while(opcao != 4){
+		switch(opcao){
+			case 1:
+				printf("\nDeseja usar a imagem predeterminada?(\n");
+				printf("1) Sim\n");
+				printf("2) Nao\n");
+				printf("Digite sua opcao: ");
+				scanf("%d", &escolha);
+				
+				if (escolha == 1){
+					figura = load_from_ppm("a.ppm");
+				}
+				else{
+					printf("\nDigite o nome do arquivo que deseja usar: ");
+					scanf("%s", arquivo);
+					figura = load_from_ppm(arquivo);
+				}
+				break;
+			case 2:
+				rgb_to_gray(figura, cinza);
+				break;
+			case 3:
+				printf("\nDigite o nome que deseja colocar na imagem: ");
+				scanf("%s", arquivo);
+				write_to_ppm(figura, arquivo);
+				break;
+		}
+		printf("Escolha sua opcao: \n");
+		printf("1) Carregar imagem\n" );
+		printf("2) Converter imagem para cinza\n" );
+		printf("3) Escrever imagem\n" );
+		printf("4) Sair\n" );
+		printf("Digite sua opcao: ");
+		scanf("%d", &opcao);
+	}
 
 	return 0;
 }
